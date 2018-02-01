b84f4c9b7e46a4ba8b4bcdc0404aae30a55fa629
Oleksandr Grechanov <oleksandr.grechanov@cgm.com>
2018-02-01 15:42:02 +0200

    remove "import static"

--- customer-service/src/test/java/com/example/microservices/customer/config/SpringTestConfig.java
+++ customer-service/src/test/java/com/example/microservices/customer/config/SpringTestConfig.java
@@ -1,16 +1,15 @@
 package com.example.microservices.customer.config;
 
 import com.example.microservices.customer.intercomm.AccountClient;
+import org.mockito.Mockito;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import static org.mockito.Mockito.mock;
 
 @Configuration
 public class SpringTestConfig {
 
     @Bean
     public AccountClient accountClient() {
-        return mock(AccountClient.class);
+        return Mockito.mock(AccountClient.class);
     }
 }
--- customer-service/src/test/java/com/example/microservices/customer/controllers/CustomerControllerTest.java
+++ customer-service/src/test/java/com/example/microservices/customer/controllers/CustomerControllerTest.java
@@ -8,15 +8,13 @@ import com.google.common.collect.Lists;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.json.JacksonTester;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
-import static org.mockito.Mockito.reset;
-import static org.mockito.Mockito.when;
 
 public class CustomerControllerTest extends BaseControllerIT {
 
@@ -28,7 +26,7 @@ public class CustomerControllerTest extends BaseControllerIT {
 
     @After
     public void cleanup() {
-        reset(accountClient);
+        Mockito.reset(accountClient);
     }
 
     @Test
@@ -36,7 +34,7 @@ public class CustomerControllerTest extends BaseControllerIT {
 
         // init
         ArrayList<Account> accounts = Lists.newArrayList( new Account(1, "11111") );
-        when(accountClient.getAccounts(1)).thenReturn(accounts);
+        Mockito.when(accountClient.getAccounts(1)).thenReturn(accounts);
 
         // test
         String body = this.restTemplate.getForObject("/customers/1", String.class);
